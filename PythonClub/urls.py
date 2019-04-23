9e291575f49bea97dcc20f00615505c31e8898cf
VazzolerMatheus <vazzoler.matheus@gmail.com>
2019-04-22 18:07:47 -0700

    Changes class 2 and 3

--- PythonClub/urls.py
+++ PythonClub/urls.py
@@ -13,10 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.urls import url
-from django.contrib import admin
-
-from . import views
+from django.urls import path
+from PythonApp import views
 
 urlpatterns=[
 
