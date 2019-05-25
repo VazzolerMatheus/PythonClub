4ef8dd89d18ea30096994aa83a86dde643da6269
Vazzoler <vazzoler.matheus@gmail.com>
2019-05-24 17:11:11 -0700

    Assignment 8 - Forms

--- PythonApp/urls.py
+++ PythonApp/urls.py
@@ -4,18 +4,25 @@ from . import views
 
 urlpatterns=[
 
-
-			#index is a function inside the view.py file
-   path('', views.index, name='index'),
+   #index is a function inside the view.py file
+   path('PythonApp/', views.index, name='index'),
 
    path('getResources/', views.getResources, name='resources'),
 
    path('getMeetings/', views.getMeetings, name='meetings'),
 
-   path('meetingdetails/<int:id>', views.meetingDetails, name='meetingdetails')
-  
-]
+   path('meetingdetails/<int:id>/', views.meetingDetails, name='meetingdetails'),
 
+   #url for the forms NewResouce
+   path('newresource/', views.newResource, name="newresource")
+  	
+  	#First atribute on Path("ex/") is just the directory that will appear on the navigation bar
+
+  	# By assigning the url a name you can use this value as a reference in view methods and templates, 
+	# which means any future changes made to the url regular expression, automatically update all url definitions 
+	# in view methods and templates.
+
+]
 
 
 
